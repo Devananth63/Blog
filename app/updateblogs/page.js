7e7bd1fbf10087d95af28bdef71f2667bf0a72cb
Devananth63 <devananth.ss2023cce@sece.ac.in>
2025-07-18 15:17:02 +0530

    Updated all files and fixed import paths

--- app/updateblogs/page.js
+++ app/updateblogs/page.js
@@ -1,5 +1,6 @@
 import { Suspense } from 'react'; // Optional, use only if you want to add suspense boundary
-import EditBlogForm from '../../Components/EditBlogForm';  // Correct path
+import EditBlogForm from '../Components/EditBlogForm';  // Note the capital 'C' in Components
+
 
 export default function UpdateBlogPage() {
   return (
